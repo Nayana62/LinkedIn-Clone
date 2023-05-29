f7a3bbdc08d6c973d31d1eea4ccfaf7b8406b981
Nayana62 <nmnayana62@gmail.com>
2023-05-29 08:45:12 +0530

    modified: footer.css

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
-import React, { useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux'
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Feed from './Feed';
-import { login, logout, selectUser } from './features/userSlice';
-import Login from './Login';
-import { auth } from './firebase';
-import Widgets from './Widgets';
-import Footer from './Footer';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import { login, logout, selectUser } from "./features/userSlice";
+import Login from "./Login";
+import { auth } from "./firebase";
+import Widgets from "./Widgets";
+import Footer from "./Footer";
 
 function App() {
-
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    auth.onAuthStateChanged(userAuth => {
-      if(userAuth){
-        dispatch(login({
-          email: userAuth.email,
-          uid:userAuth.uid,
-          displayName: userAuth.displayName,
-          photoUrl: userAuth.photoURL,
-        }))
-      }else{
-        dispatch(logout())
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
       }
     });
   }, []);
 
   return (
     <div className="app">
-
-      <Header/>
-
-      {!user ? (<Login/>) : 
-      
-      <div className='app__body'>
-      <Sidebar/>
-      <Feed/>
-      <Widgets/>
-      
-      </div>
-      }
-
-<Footer/>
-
-     
+      {!user ? (<Login />) : (
+        <div className="app__body">
+          <Header />
+          <Sidebar />
+          <Feed />
+          <Widgets />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,64 +1,59 @@
-import React from 'react';
-import './Header.css';
-import HeaderOption from './HeaderOption';
+import React from "react";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
 
-import SearchIcon from '@mui/icons-material/Search';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import HomeIcon from '@mui/icons-material/Home';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { useDispatch } from 'react-redux';
-import { logout } from './features/userSlice';
-import { auth } from './firebase';
-
-
+import SearchIcon from "@mui/icons-material/Search";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function Header() {
-
   const dispatch = useDispatch();
 
   const logoutOfApp = () => {
-    dispatch(logout())
+    dispatch(logout());
     auth.signOut();
+  };
+
+  if(window.location.pathname === './Login.js') {
+    console.log('we are in the login page')
   }
 
   return (
-    <div className='header'>
-       
-    <div className='header__left'>
-        <LinkedInIcon className='logo' style={{color:"blue" , fontSize: "40px"}} />
-
-        <div className='header__search'>
-            <SearchIcon/>
-            <input placeholder='Search' type='text'/>
-        </div>
-    </div>
-
-     <div className='header__right'>
-    <div className='links'>
-        <HeaderOption Icon={HomeIcon}  title='Home'/>
-        <HeaderOption Icon={SupervisorAccountIcon}  title='My Network'/>
-        <HeaderOption Icon={BusinessCenterIcon}  title='Jobs'/>
-        <HeaderOption Icon={ChatIcon}  title='Messaging'/>
-        <HeaderOption Icon={NotificationsIcon}  title='Notifications'/>
-    </div>
-
-    <div className="profile">
-    <HeaderOption 
-        avatar= {true}
-        title='Me' 
-        onClick={logoutOfApp}
+    <div className="header">
+      <div className="header__left">
+        <LinkedInIcon
+          className="logo"
+          style={{ color: "blue", fontSize: "40px" }}
         />
-    </div>
 
-    </div>
+        <div className="header__search">
+          <SearchIcon />
+          <input placeholder="Search" type="text" />
+        </div>
+      </div>
 
+      <div className="header__right">
+        <div className="links">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+          <HeaderOption Icon={ChatIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        </div>
 
-        
+        <div className="profile">
+          <HeaderOption avatar={true} title="Me" onClick={logoutOfApp} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
